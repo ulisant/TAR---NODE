df0389cc533b3ad2069942cfb6b58367222e092b
ulisant <Ulises@MacBook-Pro-de-mac.local>
2016-08-18 17:05:32 -0500

    modify routes

--- config/routes.js
+++ config/routes.js
@@ -34,7 +34,7 @@ module.exports.routes = {
 
   '/': 'HomeController.getPosts',
   '/webhook': 'BotController.botTar',
-  'post /webhook': 'BotController.msgEvents'
+  'POST /webhook': 'BotController.msgEvents'
 
 
   /***************************************************************************
